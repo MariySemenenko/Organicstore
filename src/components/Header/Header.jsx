f1015e4d561fcd58ad0f975115f64a8e86c4e92c
MariySemenenko <mashyni92@gmail.com>
2023-06-13 22:18:03 +0300

    icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import { Nav } from "components/Styled.styled"
-
+import { Nav, Div, H1, Number, Line, Img } from "components/Styled.styled"
+import leterIcon from '../icons/leter.png';
 export const Header = () => {
 
 return (
     <Nav>
+        <H1>Order On Phone:<Number>+321 453 68 739</Number> </H1>
+        <Line></Line>
+        
+      <Img src={leterIcon} alt="Heart Icon" />
+   
         <span>♥</span>
-        <h1>Whishlist</h1>
+        <Div>Whishlist</Div>
     </Nav>
 )
 
--- src/components/Styled.styled.jsx
+++ src/components/Styled.styled.jsx
@@ -1,5 +1,39 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const Nav = styled.div`
-  color: green;
+  background-color: #f4f5f6;
+  height: 50px;
+  margin: 0;
 `;
+
+export const Div = styled.div`
+  position: fixed;
+  cursor: pointer;
+`;
+
+export const H1 = styled.h1`
+  text-transform: capitalize;
+  display: inline-block;
+  font-size: 14px;
+  color: #9c9c9c;
+  font-weight: normal;
+  margin-left: 80px;
+  font-family: 'Roboto', sans-serif;
+  cursor: pointer;
+`;
+export const Number = styled.span`
+  color: #7fb401;
+  text-transform: capitalize;
+  font-size: 14px;
+  text-transform: capitalize;
+  cursor: pointer;
+`;
+export const Line = styled.span`
+  margin-right: 10px;
+  border: 1px dashed #9c9c9c;
+`;
+export const Img = styled.img`
+  color:#9c9c9c;
+  width: 35px;
+  
+`
