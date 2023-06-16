24075884c702b80568229a27245d2e2079ed576c
MariySemenenko <mashyni92@gmail.com>
2023-06-16 11:45:50 +0300

    position

--- src/components/AnimatedImages/AnimatedImages.jsx
+++ src/components/AnimatedImages/AnimatedImages.jsx
@@ -1,28 +1,31 @@
 import { motion } from 'framer-motion';
-import '../AnimatedImages.css'
+import '../AnimatedImages.css';
 
 export const AnimatedImages = () => {
-    const images = [
-      'image1.jpg',
-      'image2.jpg',
-      'image3.jpg',
-      'image4.jpg',
-      'image5.jpg',
-      'image6.jpg',
-    ];
-    return (
-        <div className="animated-images-container">
-          {images.map((image, index) => (
-            <motion.img
-              key={index}
-              className="animated-image"
-              src={`path/to/${image}`}
-              alt={`Animated Image ${index + 1}`}
-              initial={{ x: '100%' }}
-              animate={{ x: 0 }}
-              transition={{ duration: 1, delay: index * 0.2 }}
-            />
-          ))}
-        </div>
-      );
-    };
+  const images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    'image4.jpg',
+    'image5.jpg',
+    'image6.jpg',
+  ];
+  return (
+    <div className="animated-images-container">
+      {images.map((image, index) => (
+        <motion.img
+          key={index}
+          className="animated-image"
+          src={`path/to/${image}`}
+          alt={`Animated Image ${index + 1}`}
+          initial={{ x: -100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1, delay: index * 0.2 }}
+        />
+      ))}
+    </div>
+  );
+};
+
+
+//initial={{ x: "100%" }} якщо потрібно анімацію з правої сторони
