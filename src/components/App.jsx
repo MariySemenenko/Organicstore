9ff0a6bf5c27dc024c404186889c05145a2445dd
MariySemenenko <mashyni92@gmail.com>
2023-07-04 22:15:24 +0300

    productList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,25 @@
-// import React from 'react';
-// import { Header } from './Header/Header';
-// import { WishlistModal } from './WishlistModal/WishlistModal';
-
-
-// export const App = () => {
-//   return (
-//     <div>
-//       <Header>
-//         <WishlistModal />
-//       </Header>
-//     </div>
-//   );
-// };
-
-///////////////////////////////////////////////////////////
-
-import React from 'react';
 import { Header } from './Header/Header';
-//import { WishlistModal } from './WishlistModal/WishlistModal';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import { WishlistPage } from '../pages/wishlistPage/WishlistPage';
+import { ProductList } from 'featchProductList';
+
+
+
 
 export const App = () => {
+
+
   return (
-   
-//  <div> 
-//         <Header>
-//         <WishlistModal />
-//        </Header>
-//     </div> 
-
-
-
-
-      <Routes>
-        <Route path="/" element={<Header />}/>
-          {/* <Route index element={<WishlistModal />} /> */}
-          {/* Маршрут для сторінки "Whishlist" */}
-          <Route path="/whishlist" element={<WishlistPage />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-   
+  <>
+    <Routes>
+      <Route path="/" element={<Header />} />
+      {/* <Route index element={<WishlistModal />} /> */}
+      {/* Маршрут для сторінки "Whishlist" */}
+      <Route path="/whishlist" element={<WishlistPage />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
+      
+    </Routes>
+    {/* { <ProductList />} */}
+    </>
   );
 };
-
-
-
-
-
-
